eeb3cfcf77bbdc26cb826f476828749d256b7df9
Bart Zonneveld <loop@superinfinite.com>
2011-02-25 09:26:18 +0000

    removing all log files from subversion

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110224134648) do
+ActiveRecord::Schema.define(:version => 20110224122830) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -57,12 +57,6 @@ ActiveRecord::Schema.define(:version => 20110224134648) do
   create_table "presets", :force => true do |t|
     t.string   "name",       :null => false
     t.text     "parameters"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  create_table "statuses", :force => true do |t|
-    t.string   "name",       :null => false
     t.datetime "created_at"
     t.datetime "updated_at"
   end
