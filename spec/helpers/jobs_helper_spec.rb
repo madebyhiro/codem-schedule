920eded5c86f69baee58a701e03bc9543acf268a
Bart Zonneveld <loop@superinfinite.com>
2011-05-04 10:18:37 +0000

    Removed specs

--- spec/helpers/jobs_helper_spec.rb
+++
@@ -1,9 +0,0 @@
-require 'spec_helper'
-
-include JobsHelper
-
-describe JobsHelper do
-  it "should return the correct new job link" do
-    new_job_link.should == link_to('Create a new job', new_job_path)
-  end
-end
