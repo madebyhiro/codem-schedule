69754909791bcec640627d3f3bf824a7c0ba0e42
Bart Zonneveld <loop@superinfinite.com>
2011-05-04 08:04:20 +0000

    New init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Scheduler::Application.config.secret_token = '01e1f01f1c36d35b6d5b40705efd8ca78619919682ab186502ef0324c904ed48168b80eb28d0c744453c1518fbf72ca767fcc7a55de2afa09b83cffd02c69f3b'
