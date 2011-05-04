4bc78a4d5fb5960438bde4383ae3021d36e8a3f8
Bart Zonneveld <loop@superinfinite.com>
2011-05-04 10:20:16 +0000

    More cleanups

--- app/helpers/api/jobs_helper.rb
+++
@@ -1,2 +0,0 @@
-module Api::JobsHelper
-end
--- app/models/job.rb
+++ app/models/job.rb
@@ -5,14 +5,14 @@ class Job < ActiveRecord::Base
   Completed   = 'complete'
   Failed      = 'failed'
   
-  after_initialize :set_initial_state  
-
   belongs_to :preset
   
   scope :recent, :order => "updated_at DESC", :limit => 10, :include => [:host, :preset]
   
   validates :source_file, :destination_file, :preset_id, :presence => true
   
+  after_initialize :set_initial_state  
+
   def self.from_api(options)
     new(:source_file => options['input'],
         :destination_file => options['output'],
