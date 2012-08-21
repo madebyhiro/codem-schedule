2291088dad6faf6ef46e4e859ae6b88473794194
Bart Zonneveld <loop@superinfinite.com>
2012-08-21 15:08:30 +0200

    Added search specs

--- app/models/job.rb
+++ app/models/job.rb
@@ -55,47 +55,57 @@ class Job < ActiveRecord::Base
     end
 
     def recents(opts={})
-      jobs = recent.page(opts[:page])
+      jobs = scoped
+
+      if opts[:query]
+        jobs = Job.search(opts[:query])
+      end
+
+      jobs = jobs.recent.page(opts[:page])
 
       if opts[:sort] && opts[:dir]
         jobs = jobs.order('jobs.' + opts[:sort] + ' ' + opts[:dir])
       end
 
-      if opts[:query]
-        opts[:query].split(' ').each do |part|
-          field,value = part.split(':')
-          value = value.to_s
+      jobs
+    end
 
-          case field
-          when /^id|^job/
-            jobs = jobs.where("jobs.id = ?", value)
-          when /^state/
-            jobs = jobs.where("state = ?", value)
-          when /^source|^input/
-            jobs = jobs.where("source_file LIKE ?", '%'+value+'%')
-          when /^dest|^output/
-            jobs = jobs.where("destination_file LIKE ?", '%'+value+'%')
-          when /^file/
-            value = '%'+value+'%'
-            jobs = jobs.where("source_file LIKE ? OR destination_file LIKE ?", value, value)
-          when /^preset/
-            value = '%'+value+'%'
-            jobs = jobs.includes(:preset).where("presets.name LIKE ?", value)
-          when /^host/
-            value = '%'+value+'%'
-            jobs = jobs.includes(:host).where("hosts.name LIKE ?", value)
-          when /^submitted/
-            if dates = val_to_date(value)
-              jobs = jobs.where("jobs.created_at BETWEEN ? AND ?", dates[0], dates[1])
-            end
-          when /^completed/
-            if dates = val_to_date(value)
-              jobs = jobs.where("jobs.completed_at BETWEEN ? AND ?", dates[0], dates[1])
-            end
-          when /^started/
-            if dates = val_to_date(value)
-              jobs = jobs.where("jobs.transcoding_started_at BETWEEN ? AND ?", dates[0], dates[1])
-            end
+    def search(query)
+      jobs = scoped
+
+      query.split(' ').each do |part|
+        field,value = part.split(':')
+        value = value.to_s
+
+        case field
+        when /^id|^job/
+          jobs = jobs.where("jobs.id = ?", value)
+        when /^state/
+          jobs = jobs.where("state = ?", value)
+        when /^source|^input/
+          jobs = jobs.where("source_file LIKE ?", '%'+value+'%')
+        when /^dest|^output/
+          jobs = jobs.where("destination_file LIKE ?", '%'+value+'%')
+        when /^file/
+          value = '%'+value+'%'
+          jobs = jobs.where("source_file LIKE ? OR destination_file LIKE ?", value, value)
+        when /^preset/
+          value = '%'+value+'%'
+          jobs = jobs.includes(:preset).where("presets.name LIKE ?", value)
+        when /^host/
+          value = '%'+value+'%'
+          jobs = jobs.includes(:host).where("hosts.name LIKE ?", value)
+        when /^submitted|^created/
+          if dates = val_to_date(value)
+            jobs = jobs.where("jobs.created_at BETWEEN ? AND ?", dates[0], dates[1])
+          end
+        when /^completed/
+          if dates = val_to_date(value)
+            jobs = jobs.where("jobs.completed_at BETWEEN ? AND ?", dates[0], dates[1])
+          end
+        when /^started/
+          if dates = val_to_date(value)
+            jobs = jobs.where("jobs.transcoding_started_at BETWEEN ? AND ?", dates[0], dates[1])
           end
         end
       end
@@ -109,7 +119,7 @@ class Job < ActiveRecord::Base
       if parsed = Chronic.parse(val)
         t0 = parsed.at_beginning_of_day
         t1 = t0 + 1.day
-        [t0,t1]
+        [t0.to_date, t1.to_date]
       else
         false
       end
