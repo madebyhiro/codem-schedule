f302b4260f1de542dc3899f041dc8e5ba3abacc6
Bart Zonneveld <bart@madebyhiro.com>
2015-09-25 16:31:30 +0200

    Handling duplicate column errors

--- db/migrate/20150916122149_add_segment_time_options_to_presets.rb
+++ db/migrate/20150916122149_add_segment_time_options_to_presets.rb
@@ -1,5 +1,6 @@
 class AddSegmentTimeOptionsToPresets < ActiveRecord::Migration
   def change
+    return if ActiveRecord::Base.connection.column_exists?(:presets, :segment_time_options)
     add_column :presets, :segment_time_options, :string
   end
 end
